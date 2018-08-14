cd0be8666543e5ac4cf2b9acfb2faf148d2a92a8
sotoncit06 <sotoncit064417@gmail.com>
2018-08-14 09:03:30 -0500

    Rest of the part

---
+++ djangoautic/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # return HttpResponse('homepage')
+    return render(request, 'HomePage.html')
+
+
+def about(request):
+    # return HttpResponse('about')
+    return render(request, 'About.html')
---
+++ venv1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\S.T.N\AppData\Local\Programs\Python\Python36-32\djangoautic\venv1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
